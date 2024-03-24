ab3b63a8d0e813d85837dfe4bb40e7dad9fa384b
Zeeshan Ali Haider <zahaider14@outlook.com>
2024-03-25 00:28:31 +0500

    upgraded

--- Assets/ProjectileMotion.cs
+++ Assets/ProjectileMotion.cs
@@ -1,4 +1,6 @@
 using DG.Tweening;
+using System;
+using System.Collections;
 using UnityEngine;
 public class ProjectileMotion : MonoBehaviour
 {
@@ -10,11 +12,15 @@ public class ProjectileMotion : MonoBehaviour
     private Vector2 initialPosition;
     private Vector2 targetPosition;
     private float timeToReachTarget;
+    public float jumpSpeed = 5f; // The speed of the jump
+    public float rotateSpeed = 5f;
+    public float jumpHeight = 5f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         transform.DOMoveX(target2.position.x, 3f);
+        //rb.DOMove();
     }
 
     public void Jump2()
@@ -34,4 +40,37 @@ public class ProjectileMotion : MonoBehaviour
         // Set initial velocity of the rigidbody
         rb.velocity = new Vector2(horizontalVelocity, verticalVelocity);
     }
+
+    public void JumpBetweenPoints(Transform[] targetPositions, Action onJumpComplete = null)
+    {
+        StartCoroutine(JumpSequence(targetPositions, onJumpComplete));
+    }
+
+    private IEnumerator JumpSequence(Transform[] targetPositions, Action onJumpComplete)
+    {
+        foreach (Transform targetPosition in targetPositions)
+        {
+            transform.DOTogglePause();
+            initialPosition = transform.position;
+
+            // Calculate time to reach target using projectile motion equation
+            float displacementY = targetPosition.position.y - initialPosition.y;
+            float verticalVelocity = Mathf.Sqrt(-2 * gravity * displacementY);
+            float timeToReachTarget = -verticalVelocity / gravity;
+
+            // Calculate initial velocity using time to reach target and horizontal displacement
+            float displacementX = targetPosition.position.x - initialPosition.x;
+            float horizontalVelocity = displacementX / timeToReachTarget;
+
+            // Set initial velocity of the rigidbody
+            rb.velocity = new Vector2(horizontalVelocity, verticalVelocity);
+
+            // Wait for the player to reach the target position
+            yield return new WaitForSeconds(timeToReachTarget);
+        }
+
+        // Invoke the callback when the jump sequence is complete
+        onJumpComplete?.Invoke();
+    }
+
 }
--- Assets/TriggerManager.cs
+++ Assets/TriggerManager.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerManager : MonoBehaviour
+
 {
+    [SerializeField]Transform[] targetPositions;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //    if(collision.transform.CompareTag("Player"))
-        collision.transform.GetComponent<ProjectileMotion>().Jump2();
+        //collision.transform.GetComponent<ProjectileMotion>().Jump2();
+        collision.transform.GetComponent<ProjectileMotion>().JumpBetweenPoints(targetPositions, null);
 
     }
 }
